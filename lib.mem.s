8dee55c54d24cc8e4467dd3e18e266a8fc27c83e
S1mon <jaforsi.kozak@gmail.com>
2025-11-23 00:20:23 +0100

    implemented mem/cpy and mem/set

---
+++ lib.mem.s
@@ -0,0 +1,78 @@
+
+
+
+var _n
+var _src
+var _dst
+
+;(dst* src* n --)
+lab mem/cpy
+    stv _n
+    stv _src
+    stv _dst
+
+lab mem/cpy/loop
+    ;loop check
+    ldv _n
+    dup
+    lit 0
+    equ
+    jcn mem/cpy/done
+    lit 1
+    sub ;count down
+    stv _n
+
+    ;read from src
+    ldv _src
+    dup
+    inc
+    stv _src
+    lda
+
+    ;write to dst
+    ldv _dst
+    dup
+    inc
+    stv _dst
+    sta
+
+    jmp mem/cpy/loop
+
+lab mem/cpy/done
+    ret
+
+
+var char
+
+;(dst* char n -- )
+lab mem/set
+    stv _n
+    stv _char
+    stv _dst
+
+lab mem/set/loop
+    ;loop check
+    ldv _n
+    dup
+    lit 0
+    equ
+    jcn mem/set/done
+    lit 1
+    sub ;count down
+    stv _n
+
+    ldv _char
+
+    ldv _dst
+    dup
+    inc
+    stv _dst
+    sta
+
+    jmp mem/set/loop
+
+lab mem/set/done
+    ret
+
+
+
